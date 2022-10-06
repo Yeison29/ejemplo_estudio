2b2f46b21311b7c72a7248ff1e4db377038a2f5b
YEISON ASCANIO <ydascanioa@ufpso.edu.co>
2022-10-06 08:16:45 -0500

    Se creo la estructura basica del proyecto

---
+++ src/main/java/com/example_estudio/example_estudio/ExampleEstudioApplication.java
@@ -0,0 +1,13 @@
+package com.example_estudio.example_estudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleEstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleEstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example_estudio/example_estudio/ExampleEstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_estudio.example_estudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleEstudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
